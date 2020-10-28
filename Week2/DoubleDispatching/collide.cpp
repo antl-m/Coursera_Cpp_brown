14356b2d9e86a3f4f03d1095c29def65b1b65294
Antl_M <Antl_M@LAPTOP-5N989V8M>
2020-10-29 01:17:36 +0200

    DoubleDispatching project: passed successfully

--- Week2/DoubleDispatching/collide.cpp
+++ Week2/DoubleDispatching/collide.cpp
@@ -11,82 +11,202 @@ using namespace std;
 // Определите классы Unit, Building, Tower и Fence так, чтобы они наследовались от
 // GameObject и реализовывали его интерфейс.
 
-class Unit {
+class Unit : public GameObject {
 public:
-  explicit Unit(geo2d::Point position);
+    explicit Unit(geo2d::Point position) : pos(position) {}
+
+    virtual bool Collide(const GameObject &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Unit &that) const override {
+        return geo2d::Collide(pos, that.pos);
+    }
+
+    virtual bool CollideWith(const Building &that) const override;
+
+    virtual bool CollideWith(const Tower &that) const override;
+
+    virtual bool CollideWith(const Fence &that) const override;
+
+private:
+    geo2d::Point pos;
 };
 
-class Building {
+class Building : public GameObject {
 public:
-  explicit Building(geo2d::Rectangle geometry);
+    friend bool Unit::CollideWith(const Building &that) const;
+
+    explicit Building(geo2d::Rectangle geometry) : rec(geometry) {}
+
+    virtual bool Collide(const GameObject &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Unit &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Building &that) const override {
+        return geo2d::Collide(rec, that.rec);
+    }
+
+    virtual bool CollideWith(const Tower &that) const override;
+
+    virtual bool CollideWith(const Fence &that) const override;
+
+private:
+    geo2d::Rectangle rec;
 };
 
-class Tower {
+bool Unit::CollideWith(const Building &that) const {
+    return geo2d::Collide(pos, that.rec);
+}
+
+class Tower : public GameObject {
 public:
-  explicit Tower(geo2d::Circle geometry);
+    friend bool Unit::CollideWith(const Tower &that) const;
+
+    friend bool Building::CollideWith(const Tower &that) const;
+
+    explicit Tower(geo2d::Circle geometry) : circle(geometry) {}
+
+    virtual bool Collide(const GameObject &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Unit &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Building &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Tower &that) const override {
+        return geo2d::Collide(circle, that.circle);
+    }
+
+    virtual bool CollideWith(const Fence &that) const override;
+
+private:
+    geo2d::Circle circle;
 };
 
-class Fence {
+bool Building::CollideWith(const Tower &that) const {
+    return geo2d::Collide(rec, that.circle);
+}
+
+bool Unit::CollideWith(const Tower &that) const {
+    return geo2d::Collide(pos, that.circle);
+}
+
+class Fence : public GameObject {
 public:
-  explicit Fence(geo2d::Segment geometry);
+    friend bool Unit::CollideWith(const Fence &that) const;
+
+    friend bool Building::CollideWith(const Fence &that) const;
+
+    friend bool Tower::CollideWith(const Fence &that) const;
+
+    explicit Fence(geo2d::Segment geometry) : seg(geometry) {}
+
+    virtual bool Collide(const GameObject &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Unit &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Building &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Tower &that) const override {
+        return that.CollideWith(*this);
+    }
+
+    virtual bool CollideWith(const Fence &that) const override {
+        return geo2d::Collide(seg, that.seg);
+    }
+
+private:
+    geo2d::Segment seg;
 };
+
+bool Tower::CollideWith(const Fence &that) const {
+    return geo2d::Collide(circle, that.seg);
+}
+
+bool Building::CollideWith(const Fence &that) const {
+    return geo2d::Collide(rec, that.seg);
+}
+
+bool Unit::CollideWith(const Fence &that) const {
+    return geo2d::Collide(pos, that.seg);
+}
 
 // Реализуйте функцию Collide из файла GameObject.h
 
-bool Collide(const GameObject& first, const GameObject& second) {
+bool Collide(const GameObject &first, const GameObject &second) {
+    return first.Collide(second);
 }
 
 void TestAddingNewObjectOnMap() {
-  // Юнит-тест моделирует ситуацию, когда на игровой карте уже есть какие-то объекты,
-  // и мы хотим добавить на неё новый, например, построить новое здание или башню.
-  // Мы можем его добавить, только если он не пересекается ни с одним из существующих.
-  using namespace geo2d;
+    // Юнит-тест моделирует ситуацию, когда на игровой карте уже есть какие-то объекты,
+    // и мы хотим добавить на неё новый, например, построить новое здание или башню.
+    // Мы можем его добавить, только если он не пересекается ни с одним из существующих.
+    using namespace geo2d;
 
-  const vector<shared_ptr<GameObject>> game_map = {
-    make_shared<Unit>(Point{3, 3}),
-    make_shared<Unit>(Point{5, 5}),
-    make_shared<Unit>(Point{3, 7}),
-    make_shared<Fence>(Segment{{7, 3}, {9, 8}}),
-    make_shared<Tower>(Circle{Point{9, 4}, 1}),
-    make_shared<Tower>(Circle{Point{10, 7}, 1}),
-    make_shared<Building>(Rectangle{{11, 4}, {14, 6}})
-  };
+    const vector<shared_ptr<GameObject>> game_map = {
+            make_shared<Unit>(Point{3, 3}),
+            make_shared<Unit>(Point{5, 5}),
+            make_shared<Unit>(Point{3, 7}),
+            make_shared<Fence>(Segment{{7, 3},
+                                       {9, 8}}),
+            make_shared<Tower>(Circle{Point{9, 4}, 1}),
+            make_shared<Tower>(Circle{Point{10, 7}, 1}),
+            make_shared<Building>(Rectangle{{11, 4},
+                                            {14, 6}})
+    };
 
-  for (size_t i = 0; i < game_map.size(); ++i) {
-    Assert(
-      Collide(*game_map[i], *game_map[i]),
-      "An object doesn't collide with itself: " + to_string(i)
-    );
+    for (size_t i = 0; i < game_map.size(); ++i) {
+        Assert(
+                Collide(*game_map[i], *game_map[i]),
+                "An object doesn't collide with itself: " + to_string(i)
+        );
 
-    for (size_t j = 0; j < i; ++j) {
-      Assert(
-        !Collide(*game_map[i], *game_map[j]),
-        "Unexpected collision found " + to_string(i) + ' ' + to_string(j)
-      );
+        for (size_t j = 0; j < i; ++j) {
+            Assert(
+                    !Collide(*game_map[i], *game_map[j]),
+                    "Unexpected collision found " + to_string(i) + ' ' + to_string(j)
+            );
+        }
     }
-  }
 
-  auto new_warehouse = make_shared<Building>(Rectangle{{4, 3}, {9, 6}});
-  ASSERT(!Collide(*new_warehouse, *game_map[0]));
-  ASSERT( Collide(*new_warehouse, *game_map[1]));
-  ASSERT(!Collide(*new_warehouse, *game_map[2]));
-  ASSERT( Collide(*new_warehouse, *game_map[3]));
-  ASSERT( Collide(*new_warehouse, *game_map[4]));
-  ASSERT(!Collide(*new_warehouse, *game_map[5]));
-  ASSERT(!Collide(*new_warehouse, *game_map[6]));
+    auto new_warehouse = make_shared<Building>(Rectangle{{4, 3},
+                                                         {9, 6}});
+    ASSERT(!Collide(*new_warehouse, *game_map[0]));
+    ASSERT(Collide(*new_warehouse, *game_map[1]));
+    ASSERT(!Collide(*new_warehouse, *game_map[2]));
+    ASSERT(Collide(*new_warehouse, *game_map[3]));
+    ASSERT(Collide(*new_warehouse, *game_map[4]));
+    ASSERT(!Collide(*new_warehouse, *game_map[5]));
+    ASSERT(!Collide(*new_warehouse, *game_map[6]));
 
-  auto new_defense_tower = make_shared<Tower>(Circle{{8, 2}, 2});
-  ASSERT(!Collide(*new_defense_tower, *game_map[0]));
-  ASSERT(!Collide(*new_defense_tower, *game_map[1]));
-  ASSERT(!Collide(*new_defense_tower, *game_map[2]));
-  ASSERT( Collide(*new_defense_tower, *game_map[3]));
-  ASSERT( Collide(*new_defense_tower, *game_map[4]));
-  ASSERT(!Collide(*new_defense_tower, *game_map[5]));
-  ASSERT(!Collide(*new_defense_tower, *game_map[6]));
+    auto new_defense_tower = make_shared<Tower>(Circle{{8, 2}, 2});
+    ASSERT(!Collide(*new_defense_tower, *game_map[0]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[1]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[2]));
+    ASSERT(Collide(*new_defense_tower, *game_map[3]));
+    ASSERT(Collide(*new_defense_tower, *game_map[4]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[5]));
+    ASSERT(!Collide(*new_defense_tower, *game_map[6]));
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestAddingNewObjectOnMap);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestAddingNewObjectOnMap);
+    return 0;
 }
