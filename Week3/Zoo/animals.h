f1258c1161d92377b91c72ae373f9b59b2052639
Antl_M <Antl_M@LAPTOP-5N989V8M>
2020-11-03 23:30:48 +0200

    Zoo project: passed successfully

---
+++ Week3/Zoo/animals.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <ostream>
+
+class Animal {
+public:
+  virtual std::string Voice() const {
+    return "Not implemented yet";
+  }
+  virtual ~Animal() = default;
+};
+
+
+class Tiger: public Animal {
+  std::string Voice() const override {
+    return "Rrrr";
+  }
+};
+
+
+class Wolf: public Animal {
+  std::string Voice() const override {
+    return "Wooo";
+  }
+};
+
+
+class Fox: public Animal {
+  std::string Voice() const override {
+    return "Tyaf";
+  }
+};
+
